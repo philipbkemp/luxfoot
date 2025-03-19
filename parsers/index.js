a28456e9ccf813e37d75cfc9fad13d2272a9aa4a
Phil Kemp <boondock.dragon@gmail.com>
2025-03-19 10:33:01 +0100

    Homepage

---
+++ parsers/index.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+	
+	$.ajax({
+		url: "data/seasons.json",
+		success: function(data) {
+			parseSeasons(data);
+		}
+	})
+
+});
+
+function parseSeasons(data) {
+	data.forEach(season=>{
+
+		keys = "|" + Object.keys(season).join("|");
+
+		listGroupItem = $("<DIV></DIV>").addClass("list-group-item");
+		row = $("<DIV></DIV>").addClass("row");
+		
+		colSeason = $("<DIV></DIV>").addClass("col-1").html(season.season);
+		keys = keys.replace("|season","");
+		row.append(colSeason);
+
+		if ( season.note ) {
+			colNote = $("<DIV></DIV>").addClass("col-11").addClass("fst-italic").html(season.note);
+			keys = keys.replace("|note","");
+			row.append(colNote);
+		}
+
+		if ( season.leagues ) {
+			keys = keys + "|leagues."+ Object.keys(season.leagues).join("|leagues.");
+
+			colLeague = $("<DIV></DIV>").addClass("col-3");
+
+			if ( season.leagues.level_1 ) {
+				colLeague.append( buildLeague(season.leagues.level_1,"leagues.level_1") );
+				keys = keys.replace("|leagues.level_1","");
+			}
+
+			if ( season.leagues.level_2 ) {
+				colLeague.append( buildLeague(season.leagues.level_2,"leagues.level_2") );
+				keys = keys.replace("|leagues.level_2","");
+			}
+
+			if ( season.leagues.level_3 ) {
+				colLeague.append( buildLeague(season.leagues.level_3,"leagues.level_3") );
+				keys = keys.replace("|leagues.level_3","");
+			}
+
+			keys = keys.replace("|leagues","");
+
+			row.append(colLeague);
+		}
+
+		listGroupItem.append(row);
+
+		$("#seasons").prepend(listGroupItem);
+
+		if ( keys.length !== 0 ) {
+			console.warn(keys);
+			console.log(season);
+		}
+
+	});
+}
+
+function buildLeague(league,name) {
+	prefix = "|" + name + ".";
+	leagueKeys = prefix+ Object.keys(league).join(prefix);
+
+	ret = "";
+
+	if ( ! league.missing ) {
+
+		if ( ! league.series ) {
+
+			ret = $("<A></A>").addClass("d-block").addClass("p-1");
+			ret.attr("href","league.html?season="+league.season+"&level="+league.level);
+			leagueKeys = leagueKeys.replace(prefix+"level","");
+			leagueKeys = leagueKeys.replace(prefix+"season","");
+			ret.html(league.name);
+			leagueKeys = leagueKeys.replace(prefix+"name","");
+
+		} else {
+
+			ret = $("<SPAN></SPAN>").addClass("d-block").addClass("p-1").html(league.name);
+			leagueKeys = leagueKeys.replace(prefix+"name","");
+			leagueKeys = leagueKeys.replace(prefix+"season","");
+			leagueKeys = leagueKeys.replace(prefix+"level","");
+
+			seriesPrefix = prefix + "series.";
+			league.series.forEach(series=>{
+				seriesKeys = seriesPrefix+ Object.keys(series).join(seriesPrefix);
+
+				retSeries = $("<A></A>").addClass("d-block").addClass("p-1").addClass("ps-5");
+				retSeries.attr("href","league.html?season="+league.season+"&level="+series.level+"&series="+series.series);
+				seriesKeys = seriesKeys.replace(seriesPrefix+"level","");
+				seriesKeys = seriesKeys.replace(seriesPrefix+"series","");
+				seriesKeys = seriesKeys.replace(seriesPrefix+"season","");
+				retSeries.html(series.name);
+				seriesKeys = seriesKeys.replace(seriesPrefix+"name","");
+
+				// winner not shown
+				seriesKeys = seriesKeys.replace(seriesPrefix+"winner","");
+
+				ret.append(retSeries);
+
+				if ( seriesKeys.length !== 0 ) {
+					console.warn(seriesKeys);
+					console.log(league);
+				}
+			});
+
+			leagueKeys = leagueKeys.replace(prefix+"series","");
+		}
+
+	} else {
+		leagueKeys = leagueKeys.replace(prefix+"missing","");
+		ret = league.name + " <small class='fst-italic'>(No data)</small>";
+		leagueKeys = leagueKeys.replace(prefix+"name","");
+		leagueKeys = leagueKeys.replace(prefix+"level","");
+		leagueKeys = leagueKeys.replace(prefix+"season","");
+	}
+
+	// winner not shown
+	leagueKeys = leagueKeys.replace(prefix+"winner","");
+
+	if ( leagueKeys.length !== 0 ) {
+		console.warn(leagueKeys);
+		console.log(league);
+	}
+
+	return ret;
+}
