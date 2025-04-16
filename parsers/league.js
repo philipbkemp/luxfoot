686df249c67e2fe18947656301ac8a5c2fa8d8f5
Phil Kemp <boondock.dragon@gmail.com>
2025-04-16 16:12:10 +0200

    Inital standings table skeleton

--- parsers/league.js
+++ parsers/league.js
@@ -63,6 +63,7 @@ function buildTabPanel(code,active=false) {
 	panel = $("<DIV></DIV>")
 		.addClass("tab-pane")
 		.addClass("fade")
+		.addClass("pt-4")
 		.attr("id",code+"-tab-pane")
 		.attr("role","tabpanel")
 		.attr("aria-labelledby",code+"-tab")
@@ -88,7 +89,7 @@ function parseLeague(league) {
 	if ( league.standings ) {
 		$("#leagueTabs").append(buildTabButton("standings","Standings",true));
 		standingsPanel = buildTabPanel("standings",true);
-		standingsPanel.html("final standings");
+		standingsPanel.append( buildStandings(league.standings) );
 		$("#leagueTabContent").append(standingsPanel);
 	}
 	if ( league.matches ) {
@@ -113,4 +114,82 @@ function parseLeague(league) {
 
 	$(".placeholder-glow").addClass("d-none");
 	$(".displayAfterLoad").removeClass("d-none");
+}
+
+function buildStandings(standings) {
+	tbl = $("<TABLE></TABLE>").addClass("table").addClass("table-sm");
+
+	thead = $("<THEAD></THEAD>");
+	theadRow = $("<TR></TR>");
+	theadRow
+		.append( $("<TH></TH>").attr("scope","col").html("#") )
+		.append( $("<TH></TH>").attr("scope","col").html("Team") )
+		.append( $("<TH></TH>").attr("scope","col").html("P") )
+		.append( $("<TH></TH>").attr("scope","col").html("W") )
+		.append( $("<TH></TH>").attr("scope","col").html("D") )
+		.append( $("<TH></TH>").attr("scope","col").html("L") )
+		.append( $("<TH></TH>").attr("scope","col").html("F") )
+		.append( $("<TH></TH>").attr("scope","col").html("A") )
+		.append( $("<TH></TH>").attr("scope","col").html("Pts") )		
+		.append( $("<TH></TH>").attr("scope","col").html("GD") )
+		.append( $("<TH></TH>").attr("scope","col").html("") )
+		;
+	thead.append(theadRow);
+
+	tbody = $("<TBODY></TBODY>");
+
+	standings.forEach(s=>{
+		subKeys = Object.keys(s);
+		for ( i=0 ; i!==subKeys.length ; i++ ) {
+			subKeys[i] = "standings." + subKeys[i];
+		}
+		addKeys(subKeys);
+
+		thisRow = $("<TR></TR>");
+		thisRow
+			.append( $("<TD></TD>").html( s.place) )
+			.append( $("<TH></TH>").attr("scope","row"),html(allTeams[s.team]) )
+			;
+		removeKey("standings.place");
+		removeKey("standings.team");
+
+		tbody.append(thisRow);
+	});
+
+	tbl.append(thead).append(tbody);
+
+	return tbl;
+
+	/*
+
+	<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>John</td>
+      <td>Doe</td>
+      <td>@social</td>
+    </tr>
+  </tbody>
+</table>*/
 }
