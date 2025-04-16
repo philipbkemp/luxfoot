6c7b4a6ee137fe11d184be06ab246272cf537a8c
Phil Kemp <boondock.dragon@gmail.com>
2025-04-16 15:28:48 +0200

    Tab framework

--- parsers/league.js
+++ parsers/league.js
@@ -30,11 +30,81 @@ function doneParsingTeams() {
 	})
 }
 
+function buildTabButton(code,label,active=false) {
+	tabBtn = $("<LI></LI>")
+		.addClass("nav-item")
+		.attr("role","presentation")
+		;
+	
+	tabBtnBtn = $("<BUTTON></BUTTON>")
+		.addClass("nav-link")
+		.attr("id",code+"-tab")
+		.attr("data-bs-toggle","tab")
+		.attr("data-bs-target","#"+code+"-tab-pane")
+		.attr("type","button")
+		.attr("role","tab")
+		.attr("aria-controls",code+"-tab-pane")
+		.html(label)
+		;
+
+	if ( active ) {
+		tabBtnBtn
+			.addClass("active")
+			.attr("aria-selected","true")
+			;
+	}
+
+	tabBtn.append(tabBtnBtn);
+
+	return tabBtn;
+}
+
+function buildTabPanel(code,active=false) {
+	panel = $("<DIV></DIV>")
+		.addClass("tab-pane")
+		.addClass("fade")
+		.id(code+"-tab-pane")
+		.attr("role","tabpanel")
+		.attr("aria-labelledby",code+"-tab")
+		.attr("tabindex",0)
+		;
+	
+	if ( active ) {
+		panel
+			.addClass("show")
+			.addClass("active")
+			;
+	}
+
+	return panel;
+}
+
 function parseLeague(league) {
 	addKeys(Object.keys(league));
 
 	setTitles(league.season,league.name);
     removeKey("season");
+
+	if ( league.standings ) {
+		$("#leagueTabs").append(buildTabButton("standings","Standings",true));
+		standingsPanel = buildPanel("standings",true);
+		standingsPanel.html("final standings");
+		$("#leagueTabContent").append(standingsPanel);
+	}
+	if ( league.matches ) {
+		$("#leagueTabs").append(buildTabButton("matches","Results Table"));
+		matchesPanel = buildPanel("matches",true);
+		matchesPanel.html("ergebniskasten");
+		$("#leagueTabContent").append(matchesPanel);
+	}
+	if ( league.playoffs ) {
+		if ( league.playoffs.relegation ) {
+			$("#leagueTabs").append(buildTabButton("po_relegation","Relegation play-off"));
+			poRelegation = buildPanel("po_relegation",true);
+			poRelegation.html("relegation play-off");
+			$("#leagueTabContent").append(poRelegation);
+		}
+	}
 
 	if ( keys.length !== 0 ) {
 		console.log(league);
