12a63cc0e89d5881b0db3326a02dbfc859b7ccf0
Phil Kemp <boondock.dragon@gmail.com>
2025-04-23 17:39:41 +0200

    2011-12 / Cup / FLF

---
+++ parsers/cup.js
@@ -0,0 +1,48 @@
+resultNotes = [];
+
+$(document).ready(function(){
+
+	if ( checkParams(["season","comp"]) ) {
+	
+		$.ajax({
+			url: "data/teams.json",
+			success: function(data) {
+				parseTeams(data);
+			}
+		});
+
+	}
+
+});
+
+function doneParsingTeams() {
+	comp = urlParams.comp;
+
+	$.ajax({
+		url: "seasons/"+urlParams.season+"/cup_"+comp+".json",
+		success: function(data) {
+			parseCup(data);
+		},
+		error: function(data) {
+			invalid();
+		}
+	})
+}
+
+function parseLeague(cup) {
+	addKeys(Object.keys(cup));
+
+	setTitles(cup.season,cup.name);
+    removeKey("season");
+	removeKey("name");
+
+    // do stuff
+
+	if ( keys.length !== 0 ) {
+		console.log(cup);
+		console.log(keys);
+	}
+
+	$(".placeholder-glow").addClass("d-none");
+	$(".displayAfterLoad").removeClass("d-none");
+}
