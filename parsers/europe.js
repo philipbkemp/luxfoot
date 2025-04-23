d06d8f3eac849d43182848697cc5f9ce4ad7c21a
Phil Kemp <boondock.dragon@gmail.com>
2025-04-23 10:09:55 +0200

    1963-64 / Europe / Cup Winners Cup

---
+++ parsers/europe.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	if ( checkParams(["season","comp"]) ) {
+	
+		$.ajax({
+			url: "data/teams.json",
+			success: function(data) {
+				parseTeams(data);
+			}
+		});
+
+	}
+
+});
+
+function doneParsingTeams() {
+	$.ajax({
+		url: "seasons/"+urlParams.season+"/europ.json",
+		success: function(data) {
+			parseEurope(data);
+		},
+		error: function(data) {
+			invalid();
+		}
+	})
+}
+
+function parseEurope(data) {
+    console.log( data[urlParams.comp] );
+}
