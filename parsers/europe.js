0f414cc9387302ffb5a2bc92a0782cd2ed1c431c
Phil Kemp <boondock.dragon@gmail.com>
2025-04-23 10:29:00 +0200

    Europe / titles

--- parsers/europe.js
+++ parsers/europe.js
@@ -26,5 +26,47 @@ function doneParsingTeams() {
 }
 
 function parseEurope(data) {
-    console.log( data[urlParams.comp] );
+    if ( ! data[urlParams.comp] ) {
+        invalid();
+    } else {
+        parseEuropeanCompetition(data[urlParams.comp]);
+    }
+}
+
+function parseEuropeanCompetition(comp) {
+    addKeys(Object.keys(comp));
+
+    setTitles(comp.season,comp.name);
+    removeKey("season");
+    removeKey("name");
+
+    $("#menu-links").append(
+		$("<LI></LI>").append(
+			$("<A></A>").addClass("p-2").attr("href","season.html?season="+comp.season).html("Season overview")
+		)
+	);
+	if ( league.links ) {
+		linkKeys = Object.keys(league.links);
+		for ( i=0 ; i!==linkKeys.length ; i++ ) {
+			linkKeys[i] = "links." + linkKeys[i];
+		}
+		addKeys(linkKeys);
+		if ( league.links.next ) {
+			$("#menu-links").append(
+				$("<LI></LI>").append(
+					$("<A></A>").addClass("p-2").attr("href","europe.html?season="+comp.links.next+"&comp="+urlParams.comp).html("Next season")
+				)
+			);
+			removeKey("links.next");
+		}
+		if ( league.links.prev ) {
+			$("#menu-links").append(
+				$("<LI></LI>").append(
+					$("<A></A>").addClass("p-2").attr("href","europe.html?season="+comp.links.prev+"&comp="+urlParams.comp).html("Previous season")
+				)
+			);
+			removeKey("links.prev");
+		}
+		removeKey("links");
+	}
 }
--- parsers/general.js
+++ parsers/general.js
@@ -162,3 +162,58 @@ function removeKey(oldKey) {
 		keys.splice(keys.indexOf(oldKey),1);
 	}
 }
+
+function buildTabButton(code,label,active=false,missing=false) {
+	tabBtn = $("<LI></LI>")
+		.addClass("nav-item")
+		.attr("role","presentation")
+		;
+	
+	tabBtnBtn = $("<BUTTON></BUTTON>")
+		.addClass("nav-link")
+		.attr("id",code+"-tab")
+		.attr("data-bs-toggle","tab")
+		.attr("data-bs-target","#"+code+"-tab-pane")
+		.attr("type","button")
+		.attr("role","tab")
+		.attr("aria-controls",code+"-tab-pane")
+		.html(label)
+		;
+
+	if ( missing ) {
+		tabBtnBtn.addClass("opacity-25");
+	}
+
+	if ( active ) {
+		tabBtnBtn
+			.addClass("active")
+			.attr("aria-selected","true")
+			;
+	}
+
+	tabBtn.append(tabBtnBtn);
+
+	return tabBtn;
+}
+
+function buildTabPanel(code,active=false) {
+	panel = $("<DIV></DIV>")
+		.addClass("tab-pane")
+		.addClass("fade")
+		.addClass("pt-4")
+		.addClass("tab-pane__"+code)
+		.attr("id",code+"-tab-pane")
+		.attr("role","tabpanel")
+		.attr("aria-labelledby",code+"-tab")
+		.attr("tabindex",0)
+		;
+	
+	if ( active ) {
+		panel
+			.addClass("show")
+			.addClass("active")
+			;
+	}
+
+	return panel;
+}
