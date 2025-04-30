7764e9fe6359855f5291ee130964244c0b647fde
Phil Kemp <boondock.dragon@gmail.com>
2025-04-30 17:25:46 +0200

    Draw Trophies

--- parsers/clubs.js
+++ parsers/clubs.js
@@ -37,48 +37,71 @@ function parseClub(data) {
     // trophies
     if ( data.trophies ) {
         $("#theTabs").append(buildTabButton("trophies","Trophy Cabinet",isFirstTab));
-        trophyPanel = buildTabPanel("league",isFirstTab);
+        trophyPanel = buildTabPanel("trophies",isFirstTab);
 
-        console.log("TODO: trophies",data.trophies);
+        trophyPanel.append( drawTrophies(data.trophies) );
 
         $("#theTabContent").append(trophyPanel);
         isFirstTab = false;
     }
 
     // standings
-    if ( data.standings ) {
+    /*if ( data.standings ) {
         $("#theTabs").append(buildTabButton("standing","League History",isFirstTab));
-        standingsPanel = buildTabPanel("league",isFirstTab);
+        standingsPanel = buildTabPanel("standing",isFirstTab);
         $("#theTabContent").append(standingsPanel);
         console.log("TODO: standings",data.standings);
         isFirstTab = false;
-    }
+    }*/
 
     // matches
-    if ( data.matches ) {
+    /*if ( data.matches ) {
         $("#theTabs").append(buildTabButton("matches","Head-to-Head",isFirstTab));
-        matchesPanel = buildTabPanel("league",isFirstTab);
+        matchesPanel = buildTabPanel("matches",isFirstTab);
 
         console.log("TODO: matches",data.matches);
 
         $("#theTabContent").append(matchesPanel);
         isFirstTab = false;
-    }
+    }*/
 
     // europe
-    if ( data.europe ) {
+    /*if ( data.europe ) {
         $("#theTabs").append(buildTabButton("europe","European Record",isFirstTab));
-        matchesPanel = buildTabPanel("league",isFirstTab);
+        matchesPanel = buildTabPanel("europe",isFirstTab);
         
         console.log("TODO: europe",data.europe);
 
         $("#theTabContent").append(europePanel);
         $("#theTabContent").append(leaguePanel);
         isFirstTab = false;
-    }
-
+    }*/
 
     $(".placeholder-glow").addClass("d-none");
     $(".displayAfterLoad").removeClass("d-none");
     $(".showIfClub").removeClass("d-none");
+}
+
+function drawTrophies(trophies) {
+    trophy = $("<DIV></DIV>").addClass("g-4").addClass("row").addClass("row-cols-6").addClass("trophy-list");
+
+    trophies.forEach(t=>{
+        thisTrophy = $("<DIV></DIV>").addClass("col");
+        thisTrophyLink = $("<A></A>").addClass("card");
+        if ( t.cup ) {
+            thisTrophyLink.attr("href","cup.html?season"+t.season+"&comp="+t.cup)
+        } else if ( t.league ) {
+            thisTrophyLink.attr("href","league.html?season"+t.season+"&level=1")
+        }
+        thisTrophyImg = $("<DIV></DIV>").addClass("card-img").html(t.title_count);
+        thisTrophyBody = $("<DIV></DIV>").addClass("card-body");
+        thisTrophyComp = $("<STRONG></STRONG>").addClass("card-title").html(t.name);
+        thisTrophySeason =$("<P></P>").addClass("card-text").html(t.season);
+        thisTrophyBody.append(thisTrophyComp).append(thisTrophySeason);
+        thisTrophyLink.append(thisTrophyImg).append(thisTrophyBody);
+        thisTrophy.append(thisTrophyLink);
+        trophy.append(thisTrophy);
+    });
+
+    return trophy;
 }
