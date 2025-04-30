1d5e34ba2d11c8646cc89c26677b5be0e3729f12
Phil Kemp <boondock.dragon@gmail.com>
2025-04-30 16:59:33 +0200

    Placeholder tabs

--- parsers/clubs.js
+++ parsers/clubs.js
@@ -34,12 +34,51 @@ function parseClub(data) {
     $("h1 span:not(.lf)").html(data.name);
     $("title").append(" / " + data.name);
 
-    
+    isFirstTab = true;
+
     // trophies
+    if ( data.trophies ) {
+        $("#theTabs").append(buildTabButton("trophies","Trophy Cabinet",isFirstTab));
+        trophyPanel = buildTabPanel("league",isFirstTab);
+
+        console.log("TODO: trophies",data.trophies);
+
+        $("#theTabContent").append(trophyPanel);
+        isFirstTab = false;
+    }
 
     // standings
+    if ( data.standings ) {
+        $("#theTabs").append(buildTabButton("standing","League History",isFirstTab));
+        standingsPanel = buildTabPanel("league",isFirstTab);
+        $("#theTabContent").append(standingsPanel);
+        console.log("TODO: standings",data.standings);
+        isFirstTab = false;
+    }
 
     // matches
+    if ( data.matches ) {
+        $("#theTabs").append(buildTabButton("matches","Head-to-Head",isFirstTab));
+        matchesPanel = buildTabPanel("league",isFirstTab);
+
+        console.log("TODO: matches",data.matches);
+
+        $("#theTabContent").append(matchesPanel);
+        isFirstTab = false;
+    }
+
+    // europe
+    if ( data.europe ) {
+        $("#theTabs").append(buildTabButton("europe","European Record",isFirstTab));
+        matchesPanel = buildTabPanel("league",isFirstTab);
+        
+        console.log("TODO: europe",data.europe);
+
+        $("#theTabContent").append(europePanel);
+        $("#theTabContent").append(leaguePanel);
+        isFirstTab = false;
+    }
+
 
     $(".placeholder-glow").addClass("d-none");
     $(".displayAfterLoad").removeClass("d-none");
