b92ec70c8cfd6626e70e865a2f0d5eb9e066921d
Phil Kemp <boondock.dragon@gmail.com>
2025-05-02 11:17:09 +0200

    Initial Standings table for clubs

--- parsers/clubs.js
+++ parsers/clubs.js
@@ -46,13 +46,15 @@ function parseClub(data) {
     }
 
     // standings
-    /*if ( data.standings ) {
+    if ( data.standings ) {
         $("#theTabs").append(buildTabButton("standing","League History",isFirstTab));
         standingsPanel = buildTabPanel("standing",isFirstTab);
+
+        standingsPanel.append( drawStandings(data.standings) );
+
         $("#theTabContent").append(standingsPanel);
-        console.log("TODO: standings",data.standings);
         isFirstTab = false;
-    }*/
+    }
 
     // matches
     /*if ( data.matches ) {
@@ -104,4 +106,133 @@ function drawTrophies(trophies) {
     });
 
     return trophy;
+}
+
+function drawStandings(standings) {
+    tbl = $("<TABLE></TABLE>").addClass("table").addClass("table-sm").addClass("table-hover").addClass("table--standings");
+
+	thead = $("<THEAD></THEAD>");
+	theadRow = $("<TR></TR>");
+	theadRow
+		.append( $("<TH></TH>").attr("scope","col").html("") )
+		.append( $("<TH></TH>").attr("scope","col").html("Team") )
+		.append( $("<TH></TH>").attr("scope","col").html("<abbr title='Played'>P</abbr>") )
+		.append( $("<TH></TH>").attr("scope","col").html("<abbr title='Won'>W</abbr>") )
+		.append( $("<TH></TH>").attr("scope","col").html("<abbr title='Drawn'>D</abbr>") )
+		.append( $("<TH></TH>").attr("scope","col").html("<abbr title='Lost'>L</abbr>") )
+		.append( $("<TH></TH>").attr("scope","col").html("<abbr title='Goals For'>F</abbr>") )
+		.append( $("<TH></TH>").attr("scope","col").html("<abbr title='Goals Against'>A</abbr>") )
+		.append( $("<TH></TH>").attr("scope","col").html("<abbr title='Points'>Pts</abbr>") )		
+		.append( $("<TH></TH>").attr("scope","col").html("<abbr title='Goal Difference'>GD</abbr>") )
+		.append( $("<TH></TH>").attr("scope","col").html("") )
+		;
+	thead.append(theadRow);
+    
+    tbody = $("<TBODY></TBODY>");
+
+    standings.forEach(s=>{
+
+        thisRow = $("<TR></TR>");
+        thisRowNotes = $("<TD></TD>");
+
+        if ( ! s.missing ) {
+			thisRow
+				.append( $("<TD></TD>").html(s.place) )
+				.append( $("<TH></TH>").attr("scope","row").html(s.league) )
+				.append( $("<TD></TD>").html(s.w+s.d+s.l) )
+				.append( $("<TD></TD>").html(s.w) )
+				.append( $("<TD></TD>").html(s.d) )
+				.append( $("<TD></TD>").html(s.l) )
+				.append( $("<TD></TD>").html(s.f) )
+				.append( $("<TD></TD>").html(s.a) )
+				.append( $("<TD></TD>").html((s.w*ptsWin)+s.d) )
+				.append( $("<TD></TD>").html(s.f-s.a) )
+				;
+		} else {
+			thisRow
+				.append( $("<TD></TD>").html(s.place) )
+				.append( $("<TH></TH>").attr("scope","row").html(s.league) )
+				.append( $("<TD></TD>").attr("colspan",8).addClass("fst-italic").addClass("small").addClass("text-danger").addClass("text-start").html("Missing data") )
+				;
+			removeKey("standings.missing");
+		}
+        
+		
+		if ( s.champion ) {
+			thisRow.addClass("is-champion");
+		}
+	
+		if ( s.promoted ) {
+			thisRow.addClass("is-promoted");
+			if ( s.target ) {
+				
+				if ( ! s.playoff ) {
+					thisRowNotes.append(
+						$("<A></A>")
+							.html(["Promoted to",s.target.season,s.target.name].join(" "))
+							.attr("href","league.html?season="+s.target.season+"&level="+s.target.level)
+					);
+				}
+			}
+		}
+	
+		if ( s.relegated ) {
+			thisRow.addClass("is-relegated");
+			if ( s.target ) {
+
+				if ( ! s.playoff ) {
+					thisRowNotes.append(
+						$("<A></A>")
+							.html(["Relegated to",s.target.season,s.target.name].join(" "))
+							.attr("href","league.html?season="+s.target.season+"&level="+s.target.level)
+					);
+				}
+			}
+		}
+	
+		if ( s.removed ) {
+			thisRow.addClass("is-removed");
+			
+			thisRowNotes.append(
+				$("<ABBR></ABBR>")
+					.attr("title",s.removed_note)
+					.html("Removed")
+					.addClass("faux-link")
+			);
+		}
+
+		if ( s.playoff ) {
+			theText = "";
+			switch ( s.playoff ) {
+				case "downup":
+				case "relegation":
+					theText = "Relegation play-off"; break;
+				case "league_promotionplayoff":
+				case "promotion":
+				case "updown":
+					theText = "Promotion play-off"; break;
+				case "league_promotion_playoff_a":
+				case "league_promotion_playoff_b":
+					theText = "Play-off";
+					break;
+				case "title":
+					theText = "Title Decider";
+					break;
+			}
+			if ( theText !== "" ) {
+				thisRowNotes.append( $("<SPAN></SPAN>").addClass("me-2").addClass("faux-link").html(theText).on("click",function(){$("#po_"+s.playoff+"-tab").click();}) );
+				thisRow.addClass("is-playoff_"+s.playoff);
+			}
+		}
+
+		if ( s.title_count ) {
+			thisRowNotes.append( $("<SPAN></SPAN>").addClass("me-2").html( getTitleCount(s.title_count)) );
+		}
+
+		thisRow.append(thisRowNotes);
+    });
+
+    tbl.append(thead).append(tbody);
+
+    return tbl;
 }
