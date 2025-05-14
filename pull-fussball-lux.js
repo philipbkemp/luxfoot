71c210117c00d77dafefe856d5c3df4c6200f943
Phil Kemp <boondock.dragon@gmail.com>
2025-05-14 14:12:44 +0200

    2010-11 / League / 3.2

--- pull-fussball-lux.js
+++ pull-fussball-lux.js
@@ -45,7 +45,7 @@ allTeams = {
     "FC 72 Erpeldingen": "ERPL", "Blue-Boys Muhlenbach": "BBML", "US Berdorf/Consdorf": "USBC", "FC Schifflingen 95": "SCHF", "Alliance 01 Luxemburg": "ALLX",
     "RM 86 Luxemburg": "RMLX", "SC Bettembourg": "SCBT", "Titus Petingen": "UNTP", "Vistoria Rosport": "VICR", "US Mondorf": "MDLB", "FC Luxemburg City": "LUXC",
     "FC Koeppchen": "KOEP", "Rupensia Lusit. Larochette": "RPLL", "R. Heiderscheid/Eschdorf": "RHSE", "Ol. Christnach/Waldbillig": "OYCW", "FC Biekerech": "BIEK",
-    "Pratzerthal/Rédange": "FCPR", "JS Koerich": "KOSM"
+    "Pratzerthal/Rédange": "FCPR", "JS Koerich": "KOSM", "Union Mertert/Wasser.": "UNMW", "Flaxweiler/Beyren": "FBU1"
 }
 document.querySelectorAll("table table").forEach(t=>{
     t.addEventListener("click",function(e){
